31023b7be409dee8fe3c65d736ce464a2ea235c9
SEUNGHWA LEE <sw092311@cau.ac.kr>
2025-07-01 12:29:00 +0900

    springdata JPA code update

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,37 +1,20 @@
 package hello.hello_spring;
-
-import hello.hello_spring.repository.JdbcMemberRepository;
-import hello.hello_spring.repository.JdbcTemplateMemberRepository;
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.Entity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private DataSource dataSource;
-    private Entity Manager;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new MemberService(memberRepository);
     }
 }
